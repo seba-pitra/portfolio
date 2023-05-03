a51e47a448b9421fb749da369a415cb386279380
Seba <sebastian.pitra10@gmail.com>
2023-05-03 17:51:46 -0300

    styles(Home): css re-created with tailwind css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,19 @@
-import "./Home.css";
-import NavBar from "../../components/NavBar/NavBar";
-import { Linkedin } from "react-bootstrap-icons"
 import { Github } from "react-bootstrap-icons"
 import { Link } from "react-router-dom";
+import { Linkedin } from "react-bootstrap-icons"
+import "./Home.css"
 
 const Home = () => {
   return (
-    <header id="header">
-      <div className="container">
-        <h3 className="home-presentation-name" >Hi, my name is</h3>
-        <h1>Sebastian Pitra</h1>
-        <h2>I am a passionate <span>developer</span> from Argentine</h2>
+    <header className="flex h-screen justify-center items-center text-start relative ease-in-out duration-300 ">
+      <div className="">
+        <h3 className=" text-blue-400" >Hi, my name is</h3>
+        <h1 className=" text-5xl text-slate-200  m-0 p-0 leading-none font-bold font-poppins">Sebastian Pitra</h1>
+        <h2 className="text-2xl mt-5 text-slate-200">
+          I am a passionate {" "}
+          <span className="border-solid border-blue-400 border-b-2">developer</span> 
+          {" "} from Argentine
+        </h2>
 
         <nav id="navbar" className="navbar">
           <ul>
@@ -24,9 +27,13 @@ const Home = () => {
           <i className="bi bi-list mobile-nav-toggle"></i>
         </nav>
 
-        <div className="social-links">
-          <Link to={"https://www.linkedin.com/in/sebasti%C3%A1n-pitra-97b5b7248/"} target="_blank"> <Linkedin /> </Link>
-          <Link to={"https://github.com/seba-pitra"} target="_blank"> <Github /> </Link>
+        <div className="flex mt-3 -ml-4 gap-3 p-3 ">
+          <Link to={"https://www.linkedin.com/in/sebasti%C3%A1n-pitra-97b5b7248/"} target="_blank" className="bg-gray-800 p-3 rounded-middle hover:bg-gray-700 transition-animationLineBefore duration-300"> 
+            <Linkedin color="rgb(59 130 246)" size={"20px"}/> 
+          </Link >
+          <Link to={"https://github.com/seba-pitra"} target="_blank" className="bg-gray-800 p-3 rounded-middle hover:bg-gray-700 transition-animationLineBefore duration-300"> 
+            <Github color="rgb(59 130 246)" size={"20px"} /> 
+          </Link>
         </div>
       </div>
   </header>
