f2ad30eb10cd190a1a349115a8d443634600110d
Seba <sebastian.pitra10@gmail.com>
2023-05-05 15:06:50 -0300

    styles(SkillCards): responsive design fixed

--- src/components/SkillsCards/SkillsCards.jsx
+++ src/components/SkillsCards/SkillsCards.jsx
@@ -11,11 +11,11 @@ const SkillsCards = () => {
         </h2>
       </div>
 
-      <div className="flex justify-evenly flex-wrap  gap-4 mb-16 mt-3 max-[565px]:flex max-[565px]:flex-col max-[824px]:items-center ">
+      <div className="flex justify-evenly flex-wrap  gap-4 mb-16 mt-3 max-[565px]:flex max-[565px]:flex-col max-[824px]:items-center max-[824px]:mt-8">
         {/* FRONT-END */}
         <div className="max-[565px]:flex max-[565px]:flex-col max-[500px]:w-80">
-          <h1>Front End</h1>
-          <div className="flex gap-3 bg-neutral-900 p-4 rounded max-[500px]:p-1 max-[500px]:flex-col max-[500px]:items-center">
+          <h1 className=" text-2xl mb-3 max-[500px]:text-center">Front End</h1>
+          <div className="flex gap-3 bg-neutral-900 p-4 rounded max-[500px]:p-4 max-[500px]:flex-col max-[500px]:items-center">
             <div>
               <div className="flex items-center w-44 gap-2 text-lg bg-neutral-800 p-4 mt-2 mb-2 rounded-r-skill-card max-[565px]:w-40 max-[565px]:gap-2 max-[500px]:w-64 max-[500px]:flex max-[500px]:justify-start max-[500px]:text-2xl">
                 <img
@@ -192,9 +192,9 @@ const SkillsCards = () => {
         </div>
 
         {/* BACK-END */}
-        <div>
-          <h1>Back End</h1>
-          <div className="flex gap-3 bg-neutral-900 p-4 rounded max-[565px]:flex max-[565px]:flex-col max-[500px]:w-80">
+        <div className="max-[565px]:flex max-[565px]:flex-col max-[500px]:w-80  max-[500px]:mt-10">
+          <h1 className=" text-2xl mb-3 max-[500px]:text-center">Back End</h1>
+          <div  className="flex gap-3 bg-neutral-900 p-4 rounded max-[500px]:p-4 max-[500px]:flex-col max-[500px]:items-center">
             <div>
               <div className="flex items-center w-44 gap-2 text-lg bg-neutral-800 p-4 mt-2 mb-2 rounded-r-skill-card max-[565px]:w-40 max-[565px]:gap-2 max-[500px]:w-64 max-[500px]:flex max-[500px]:justify-start max-[500px]:text-2xl">
                 <img
@@ -364,9 +364,9 @@ const SkillsCards = () => {
         </div>
 
         {/* OTHERS */}
-        <div>
-          <h1>Others</h1>
-          <div className="flex flex-col gap-0 p-4 bg-neutral-900 rounded max-[565px]:flex max-[565px]:flex-col max-[500px]:w-80">
+        <div className="max-[565px]:flex max-[565px]:flex-col max-[500px]:w-80  max-[500px]:mt-10">
+          <h1 className="text-2xl mb-3 max-[500px]:text-center">Others</h1>
+          <div className="flex flex-col bg-neutral-900 p-4 rounded max-[500px]:p-4 max-[500px]:items-center">
             <div className="flex items-center w-44 gap-2 text-lg bg-neutral-800 p-4 mt-2 mb-2 rounded-r-skill-card max-[565px]:w-40 max-[565px]:gap-2 max-[500px]:w-64 max-[500px]:flex max-[500px]:justify-start max-[500px]:text-2xl">
               <svg
                 role="img"
