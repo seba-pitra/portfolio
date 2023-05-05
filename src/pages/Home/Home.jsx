19af661be48d953ea085b8b76f7de38cef78c1ff
Seba <sebastian.pitra10@gmail.com>
2023-05-05 16:17:58 -0300

    fix(Home): deleted home lin

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,11 @@
 import { Github } from "react-bootstrap-icons"
 import { Link } from "react-router-dom";
 import { Linkedin } from "react-bootstrap-icons"
-import "./Home.css"
 
 const Home = () => {
   return (
-    <header className="flex h-screen justify-center items-center text-start relative ease-in-out duration-300 ">
-      <div className="">
+    <header className="fixed inset-0 flex items-center justify-center text-start ease-in-out duration-300 ">
+      <div>
         <h3 className=" text-blue-400" >Hi, my name is</h3>
         <h1 className=" text-5xl text-slate-200  m-0 p-0 leading-none font-bold font-poppins">Sebastian Pitra</h1>
         <h2 className="text-2xl mt-5 text-slate-200">
@@ -15,14 +14,48 @@ const Home = () => {
           {" "} from Argentine
         </h2>
 
-        <nav id="navbar" className="navbar">
-          <ul>
-            <li><Link className="nav-link active" to={"/"}>Home</Link></li>
-            <li><Link className="nav-link" to={"/about"}>About</Link></li>
-            <li><Link className="nav-link" to={"/"}>Resume</Link></li>
-            <li><Link className="nav-link" to={"/"}>Services</Link></li>
-            <li><Link className="nav-link" to={"/"}>Portfolio</Link></li>
-            <li><Link className="nav-link" to={"/"}>Contact</Link></li>
+        <nav className=" mt-9 p-0">
+          <ul className=" m-0 p-0 flex justify-start w-full list-none">
+            <li className="relative">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/about"}>
+                About
+              </Link>
+            </li>
+            <li className="relative ml-8">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
+                Resume
+              </Link>
+            </li>
+            <li className="relative ml-8">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
+                Services
+              </Link>
+            </li>
+            <li className="relative ml-8">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
+                Portfolio
+              </Link>
+            </li>
+            <li className="relative ml-8">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
+                Contact
+              </Link>
+            </li>
           </ul>
           <i className="bi bi-list mobile-nav-toggle"></i>
         </nav>
