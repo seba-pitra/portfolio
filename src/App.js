b65d50248827948aa5231c43d3a42ca84d5fc570
Seba <sebastian.pitra10@gmail.com>
2023-05-05 16:21:45 -0300

    feat: added route to /projects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './pages/Home/Home';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import NavBar from './components/NavBar/NavBar';
+import Projects from './pages/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <Routes>
-          <Route exact path='/' element={<Header />} />
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/projects' element={<Projects />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -36,8 +36,8 @@ const Home = () => {
               <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
               before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
               before:bg-blue-400 before:transition-animationLineBefore 
-              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
-                Services
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/projects"}>
+                Projects
               </Link>
             </li>
             <li className="relative ml-8">
@@ -45,7 +45,7 @@ const Home = () => {
               before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
               before:bg-blue-400 before:transition-animationLineBefore 
               hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
-                Portfolio
+                Services
               </Link>
             </li>
             <li className="relative ml-8">
