9d5518e884a8c2daaf00fbc9e2e7da6bb46d77e6
Seba <sebastian.pitra10@gmail.com>
2023-05-04 22:59:56 -0300

    styles(NavBar): css changed to tailwind

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,69 @@
-import { Link, useLocation } from "react-router-dom";
-import "./NavBar.css";
+import {Link, useLocation} from "react-router-dom";
 
 const NavBar = () => {
-  const { pathname } = useLocation();
+  const {pathname} = useLocation();
 
   return (
-    <nav id="navbar" className="navbar">
-      {
-        pathname === "/"
-        ? <div></div>
-        :
+    <nav className=" mt-9 p-0">
+      {pathname === "/" ? (
+        <div></div>
+      ) : (
         <div>
-          <ul className="navbar-container"> 
-            <li><Link className="nav-link active" to={"/"}>Home</Link></li>
-            <li><Link className="nav-link" to={"/about"}>About</Link></li>
-            <li><Link className="nav-link" to={"/"}>Resume</Link></li>
-            <li><Link className="nav-link" to={"/"}>Services</Link></li>
-            <li><Link className="nav-link" to={"/"}>Portfolio</Link></li>
-            <li><Link className="nav-link" to={"/"}>Contact</Link></li>
+          <ul className=" m-0 p-0 flex justify-end w-11/12 list-none">
+            <li className="relative ml-8">
+              <Link className=" text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
+                Home
+              </Link>
+            </li>
+            <li className="relative ml-8">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/about"}>
+                About
+              </Link>
+            </li>
+            <li className="relative ml-8">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
+                Resume
+              </Link>
+            </li>
+            <li className="relative ml-8">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
+                Services
+              </Link>
+            </li>
+            <li className="relative ml-8">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
+                Portfolio
+              </Link>
+            </li>
+            <li className="relative ml-8">
+              <Link className="text-neutral-300 flex items-center justify-between p-0 text-base font-normal duration-300 
+              before:absolute before:w-0 before:h-line-title before:-bottom-1 before:left-0 
+              before:bg-blue-400 before:transition-animationLineBefore 
+              hover:before:visible hover:before:w-6 hover:text-neutral-50" to={"/"}>
+                Contact
+              </Link>
+            </li>
           </ul>
           <i className="bi bi-list mobile-nav-toggle"></i>
         </div>
-      }
+      )}
     </nav>
-  )
-}
+  );
+};
 
 export default NavBar;
