964bcadb472b31fe4ca8852266b3d594cb044c61
Seba <sebastian.pitra10@gmail.com>
2023-05-07 15:51:49 -0300

    feat: created database

---
+++ src/database/projects.js
@@ -0,0 +1,36 @@
+import dogBreedsImage from "../img/dog-breeds.jpg"
+
+export const projects =  [
+  {
+    title: "CONIN SMT",
+    image: dogBreedsImage,
+    description: "High-impact social project aimed at combating child malnutrition in Argentina. It allows users to make donations through Mercado Pago, in order to provide food and necessary resources to improve the health and well-being of affected children.", 
+    techStack: ["Javascript", "React", "Redux", "NodeJS", "Firebase"],
+    urlGithub: "https://github.com/seba-pitra/PI-DOGS",
+    urlDeploy: "https://pi-dogs-rouge-beta.vercel.app/"
+  },
+  {
+    title: "Portamor",
+    image: dogBreedsImage,
+    description: "Project focused on promoting active aging among individuals through the implementation of an online education platform in Peru. This platform provides learning opportunities and personal development to the elderly population.", 
+    techStack: ["Javascript", "React", "Redux", "NodeJS", "PostgreSQL"],
+    urlGithub: "https://github.com/seba-pitra/PI-DOGS",
+    urlDeploy: "https://pi-dogs-rouge-beta.vercel.app/"
+  },
+  {
+    title: "Non Fungible Town",
+    image: dogBreedsImage,
+    description: "An NFT Ecommerce platform that provides seamless opportunities for buying and selling NFTs using cryptocurrencies as well as cash payments.", 
+    techStack: ["Javascript", "React", "Metamask", "NodeJS", "PostgreSQL"],
+    urlGithub: "https://github.com/seba-pitra/PI-DOGS",
+    urlDeploy: "https://pi-dogs-rouge-beta.vercel.app/"
+  },
+  {
+    title: "Dog Breeds",
+    image: dogBreedsImage,
+    description: "User-friendly Single-Page-App that integrated an external dog breed API. The app empowered users to effortlessly filter, sort, and even create new dog breeds, complemented by a seamless pagination feature. This enhanced user experience enabled seamless exploration of a diverse range of dog breeds.", 
+    techStack: ["Javascript", "React", "Redux", "NodeJS", "PostgreSQL"],
+    urlGithub: "https://github.com/seba-pitra/PI-DOGS",
+    urlDeploy: "https://pi-dogs-rouge-beta.vercel.app/"
+  }
+]
