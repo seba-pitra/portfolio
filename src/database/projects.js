e3905ced82887a5ecfcbdf421c0d137ebadf94ac
Seba <sebastian.pitra10@gmail.com>
2023-05-11 10:26:22 -0300

    feat(database/projects): added images

--- src/database/projects.js
+++ src/database/projects.js
@@ -1,36 +1,37 @@
-import dogBreedsImage from "../img/dog-breeds.jpg"
+import coninImage      from "../img/conin.jpg"
+import dogBreedsImage  from "../img/dog-breeds.jpg"
+import portamorImage   from "../img/portamor.jpg"
+import nonFungibleTown from "../img/nonFungibleTown.jpg"
 
 export const projects =  [
   {
-    title: "CONIN SMT",
-    image: dogBreedsImage,
+    title:       "CONIN SMT",
+    image:        coninImage,
     description: "High-impact social project aimed at combating child malnutrition in Argentina. It allows users to make donations through Mercado Pago, in order to provide food and necessary resources to improve the health and well-being of affected children.", 
-    techStack: ["Javascript", "React", "Redux", "NodeJS", "Firebase"],
-    urlGithub: "https://github.com/seba-pitra/PI-DOGS",
-    urlDeploy: "https://pi-dogs-rouge-beta.vercel.app/"
+    techStack:   ["Javascript", "React", "Firebase", "NodeJS", "Mercado Pago"],
+    urlDeploy:   "https://embajadores-conin.vercel.app/"
   },
   {
-    title: "Portamor",
-    image: dogBreedsImage,
+    title:       "Portamor",
+    image:       portamorImage,
     description: "Project focused on promoting active aging among individuals through the implementation of an online education platform in Peru. This platform provides learning opportunities and personal development to the elderly population.", 
-    techStack: ["Javascript", "React", "Redux", "NodeJS", "PostgreSQL"],
-    urlGithub: "https://github.com/seba-pitra/PI-DOGS",
-    urlDeploy: "https://pi-dogs-rouge-beta.vercel.app/"
+    techStack:   ["Javascript", "React", "NodeJS", "PostgreSQL"],
+    urlGithub:   "https://github.com/seba-pitra/PI-DOGS",
   },
   {
-    title: "Non Fungible Town",
-    image: dogBreedsImage,
+    title:       "Non Fungible Town",
+    image:       nonFungibleTown,
     description: "An NFT Ecommerce platform that provides seamless opportunities for buying and selling NFTs using cryptocurrencies as well as cash payments.", 
-    techStack: ["Javascript", "React", "Metamask", "NodeJS", "PostgreSQL"],
-    urlGithub: "https://github.com/seba-pitra/PI-DOGS",
-    urlDeploy: "https://pi-dogs-rouge-beta.vercel.app/"
+    techStack:   ["Javascript", "React", "Metamask", "NodeJS", "Firebase"],
+    urlGithub:   "https://github.com/seba-pitra/e-commerce-NFT",
+    urlDeploy:   "https://e-commerce-nft.vercel.app/"
   },
   {
-    title: "Dog Breeds",
-    image: dogBreedsImage,
+    title:       "Dog Breeds",
+    image:       dogBreedsImage,
     description: "User-friendly Single-Page-App that integrated an external dog breed API. The app empowered users to effortlessly filter, sort, and even create new dog breeds, complemented by a seamless pagination feature. This enhanced user experience enabled seamless exploration of a diverse range of dog breeds.", 
-    techStack: ["Javascript", "React", "Redux", "NodeJS", "PostgreSQL"],
-    urlGithub: "https://github.com/seba-pitra/PI-DOGS",
-    urlDeploy: "https://pi-dogs-rouge-beta.vercel.app/"
+    techStack:   ["Javascript", "React", "Redux", "NodeJS", "PostgreSQL"],
+    urlGithub:   "https://github.com/seba-pitra/PI-DOGS",
+    urlDeploy:   "https://pi-dogs-rouge-beta.vercel.app/"
   }
 ]
