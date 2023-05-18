c33d799763d61b75956d66adcd2278ad0a7c9610
Seba <sebastian.pitra10@gmail.com>
2023-05-18 13:42:30 -0300

    fix: scroll to buttom in messages

--- src/components/ChatBot/ChatBot.tsx
+++ src/components/ChatBot/ChatBot.tsx
@@ -1,28 +1,31 @@
-import React, { useState, FC, useEffect, useRef, KeyboardEvent } from "react";
-import { useForm }       from "react-hook-form"
-import { SubmitHandler,  } from "react-hook-form"
-import { Send } from "react-bootstrap-icons";
-import Rocket from "./Rocket";
-import rocketProfileImg from "../../assets/img/rocket-profile.jpg"
+import { FC }              from "react";
+import { KeyboardEvent }   from "react";
+import React               from "react";
+import Rocket              from "./Rocket";
+import rocketProfileImg    from "../../assets/img/rocket-profile.jpg"
+import { Send }            from "react-bootstrap-icons";
+import { SubmitHandler }   from "react-hook-form"
 import TypewriterAnimation from "../TypewriterAnimation/TypewriterAnimation";
+import { useState }        from "react";
+import { useEffect }       from "react";
+import { useRef }          from "react";
+import { useForm }         from "react-hook-form"
 
-interface IFormValues {
-  content: string;
-}
 
-type Role = "user" | "assistant";
+interface IFormValues { content: string; }
 
 interface IMessages {
   id:      string;
-  role:    Role;
+  role:    "user" | "assistant";
   content: string;
 }
 
 const ChatBot: FC = () => {
   const { register, handleSubmit, reset } = useForm< IFormValues >();
 
-  const chatContainerRef = useRef<HTMLDivElement>(null);
-  
+  const chatContainerMessagesRef = useRef<HTMLDivElement>(null);
+  const chatContainerRef         = useRef<HTMLDivElement>(null);
+  const rocketContainerRef       = useRef<HTMLDivElement>(null);
   
   const [isSelected, setIsSelected] = useState< boolean >(false);
   const [messages, setMessages] = useState< Array<IMessages> >([
@@ -34,15 +37,12 @@ const ChatBot: FC = () => {
   ]);
 
   useEffect(() => {
-    scrollToBottom();
-  }, [messages.length]);
-
-  const scrollToBottom = () => {
-    const chatContainer = chatContainerRef.current;
-    if (chatContainer) {
-      chatContainer.scrollTop = chatContainer.scrollHeight;
-    }
-  };
+    !isSelected && setTimeout(() => {
+      if(rocketContainerRef.current) {
+        rocketContainerRef.current.className = ` opacity-100 duration-[0.7s] ease-in-out `;
+      } 
+    }, 2000);
+  }, [isSelected]);
 
   const onSubmit: SubmitHandler<IFormValues> = async(args) => {
     reset()
@@ -67,72 +67,114 @@ const ChatBot: FC = () => {
     })
   };
 
-  const handleKeyDown = (event: KeyboardEvent< HTMLTextAreaElement > ) => {
+  const handleKeyDown = (event: KeyboardEvent< HTMLTextAreaElement > ): void => {
     if (event.key === 'Enter') {
       event.preventDefault(); 
       onSubmit({ content: event.currentTarget.value })
     }
   }
 
+  const handleOpenChat = (): void => {
+    setTimeout(() => {
+      if(rocketContainerRef.current) {
+        rocketContainerRef.current.className = `translate-x-80 opacity-0 duration-[0.7s] ease-in-out `;
+      } 
+    }, 100)
+
+    setTimeout(() => setIsSelected(true), 500)
+
+    setTimeout(() => {
+      if(chatContainerRef.current) {
+        chatContainerRef.current.className = ` opacity-100 duration-[0.7s] ease-in-out `;
+      } 
+    }, 800)
+  };
+
+  const handleCloseChat = (): void => {
+    setTimeout(() => {
+      if(chatContainerRef.current) {
+        chatContainerRef.current.className = ` opacity-0 translate-x-80 duration-[0.7s] ease-in-out `;
+      } 
+    }, 100)
+
+    setTimeout(() => setIsSelected(false), 1000)
+  };
+
+  const scrollToBottom = () => {
+    const chatContainer = chatContainerMessagesRef.current;
+    if (chatContainer) {
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+  };
+
   return (
     <div className="fixed bottom-0 right-0 transform mr-10 mb-10">
-     {!isSelected && <div onClick={() => setIsSelected(true)} >
-        <Rocket />
-      </div>}
+     {
+        !isSelected && 
+        <div 
+          className="opacity-0"
+          ref={rocketContainerRef} 
+          onClick={handleOpenChat} > 
+          <Rocket />
+        </div>
+      }
 
       {isSelected && 
-        <>
+        <div ref={chatContainerRef} className="translate-x-80 opacity-0 duration-[0.3s] ease-in-out">
           <header className="fixed top-0 bg-neutral-800 p-2 w-full flex justify-center items-center">
             <div className="flex w-full gap-2 justify-center ">
               <img 
                 src={rocketProfileImg} 
                 alt="rocket-profile-chat"
                 className="max-h-12 rounded-middle" />
-                <div className="flex flex-col">
-                  <h1>Rocket</h1>
-                  <h2 className=" text-neutral-400">Assistant</h2>
-                </div>
-                <span
-                  onClick={() => setIsSelected(false)} 
-                  className="w-full cursor-pointer text-neutral-400 flex justify-end text-[14px]">
-                  Close chat
-                </span>
+
+              <div className="flex flex-col">
+                <h1>Rocket</h1>
+                <h2 className=" text-neutral-400">Assistant</h2>
+              </div>
+              
+              <span
+                onClick={handleCloseChat} 
+                className="w-full cursor-pointer text-neutral-400 flex justify-end text-[14px]">
+                Close chat
+              </span>
             </div>
           </header>
 
-          <div ref={chatContainerRef} className="bg-neutral-800 mt-16 text-[15px] w-[384px] h-[300px] overflow-auto p-3">
-            {messages.length && messages.map((message: IMessages, index: number) => (
-              message.role === "user" ?
-                <div key={Math.random() * index} className="flex justify-end text-end w-full p-2 mb-1">
-                  <p className="bg-blue-400 max-w-fit p-2 rounded-[3px]">
-                    {message.content}
-                  </p>
-                </div>
-                :
-                <TypewriterAnimation 
-                  isLastMessage={index === messages.length - 1}
-                  key={Math.random() * index} 
-                  word={message.content} />
-            ))}
-          </div>
+            <div ref={chatContainerMessagesRef} className="bg-neutral-800 mt-16 text-[15px] w-[384px] h-[300px] overflow-auto p-3">
+              {messages.length && messages.map((message: IMessages, index: number) => (
+                message.role === "user" ?
+                  <div key={Math.random() * index} className="flex justify-end text-end w-full p-1 mb-1">
+                    <p className="bg-blue-400 max-w-fit p-2 rounded-[3px]">
+                      {message.content}
+                    </p>
+                  </div>
+                  :
+                  <TypewriterAnimation 
+                    scrollToButtom={scrollToBottom}
+                    isLastMessage={index === messages.length - 1}
+                    key={Math.random() * index} 
+                    word={message.content} />
+              ))}
+            </div>
           
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col max-w-sm">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col max-w-sm">
 
-              <div className=" w-full flex gap-5 items-center justify-center p-4 bg-neutral-800 ">
-                <textarea
-                  onKeyDown={handleKeyDown}
-                  {...register('content', { required: true })}
-                  className=" text-neutral-200 focus:outline-none pl-2 pr-2 text-base bg-neutral-700 w-full resize-none overflow-y-auto h-[50px] max-h-[300px] pt-2 pb-2 rounded-skill-card" />
+                <div className=" w-full flex gap-5 items-center justify-center p-4 bg-neutral-800 ">
+                  <textarea
+                    onKeyDown={handleKeyDown}
+                    {...register('content', { required: true })}
+                    className=" text-neutral-200 focus:outline-none pl-2 pr-2 text-base bg-neutral-700 w-full resize-none overflow-y-auto h-[50px] max-h-[300px] pt-2 pb-2 rounded-skill-card" />
 
-                <button type="submit">
-                  <Send size={"25px"} color="#3498db" />
-                </button>
-              </div>
+                  <button type="submit">
+                    <Send size={"25px"} color="#3498db" />
+                  </button>
+                </div>
 
-          </form>
-          </>
+            </form>
+          </div>
         }
     </div>
   )
--- src/components/TypewriterAnimation/TypewriterAnimation.tsx
+++ src/components/TypewriterAnimation/TypewriterAnimation.tsx
@@ -1,13 +1,13 @@
 import React, { useState, FC, useEffect } from "react";
 import rocketImg from "../../assets/img/rocket.jpg"
 
-
 interface IProps {
   word: string;
   isLastMessage: boolean;
+  scrollToButtom: () => void;
 };
 
-const TypewriterAnimation: FC<IProps> = ({ word, isLastMessage }) => {
+const TypewriterAnimation: FC<IProps> = ({ word, isLastMessage, scrollToButtom }) => {
   const [text, setText] = useState("");
 
   useEffect(() => {
@@ -16,26 +16,26 @@ const TypewriterAnimation: FC<IProps> = ({ word, isLastMessage }) => {
     if(!isLastMessage) setText(word);
     else {
       const intervalId = setInterval(() => {
-        
         if (currentIndex === word.length - 1) {
           clearInterval(intervalId);
         } else {
           setText((prevText) => prevText + word[currentIndex])
+          scrollToButtom()
           currentIndex++;
         }
       }, 50)
   
       return () => clearInterval(intervalId);
     }      
-  }, [word, isLastMessage])
+  }, [word, isLastMessage, scrollToButtom])
 
   return (
-    <div className="flex justify-start text-start w-full p-2 mb-1">
+    <div className="flex justify-start text-start w-full p-1 mb-1">
       <img 
         src={rocketImg} 
         alt="rocket-img-chat"
         className="max-h-10" />
-      <p className="bg-neutral-500 max-w-fit p-2 rounded-[3px]">
+      <p className="bg-neutral-500 max-w-[90%] p-2 rounded-[3px]">
         {text}
       </p>
     </div>
