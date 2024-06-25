05439d3fa62a9528f124f150932148c03902652a
seba-pitra <sebastian.pitra10@gmail.com>
2024-06-25 11:19:34 -0300

    feat: update skills and CV

--- src/components/SkillsCards/SkillsCards.tsx
+++ src/components/SkillsCards/SkillsCards.tsx
@@ -1,28 +1,21 @@
-import BackEndSkillsCards  from "./BackEndSkills";
-import FrontEndSkillsCards from "./FrontEndSkills";
-import OthersSkillsCards   from "./OthersSkills";
-import React               from "react";
-
+import TechSkillsCards from "./TechSkills";
+import React from "react";
 
 const SkillsCards: React.FC = () => {
-
   return (
     <>
       <div>
-        <h2 
-        className="text-lg font-medium p-0 m-0 tracking-wide
+        <h2
+          className="text-lg font-medium p-0 m-0 tracking-wide
         uppercase text-gray-400 after:w-32 after:h-line-title after:inline-block after:bg-blue-400 
-        after:mt-1 after:mb-1 after:ml-1 after:mr-1">
+        after:mt-1 after:mb-1 after:ml-1 after:mr-1"
+        >
           Skills
         </h2>
       </div>
 
-      <div  className=" flex justify-evenly flex-wrap  gap-4 mb-16 mt-3 max-[565px]:flex max-[565px]:flex-col max-[824px]:items-center max-[824px]:mt-8">
-        <FrontEndSkillsCards />
-
-        <BackEndSkillsCards />
-
-        <OthersSkillsCards />
+      <div className=" flex justify-evenly flex-wrap  gap-4 mb-16 mt-3 max-[565px]:flex max-[565px]:flex-col max-[824px]:items-center max-[824px]:mt-8">
+        <TechSkillsCards />
       </div>
     </>
   );
--- src/database/projects.ts
+++ src/database/projects.ts
@@ -1,46 +1,48 @@
-import coninImage           from "../assets/img/conin.jpg"
-import dogBreedsImage       from "../assets/img/dog-breeds.jpg"
-import portamorImage        from "../assets/img/portamor.jpg"
-import nonFungibleTownImage from "../assets/img/nonFungibleTown.jpg"
+import coninImage from "../assets/img/conin.jpg";
+import dogBreedsImage from "../assets/img/dog-breeds.jpg";
+import portamorImage from "../assets/img/portamor.jpg";
+import nonFungibleTownImage from "../assets/img/nonFungibleTown.jpg";
 
 export interface IProject {
-  title:        string;
-  image:        string;
-  description:  string;
-  techStack:    Array<string>;
+  title: string;
+  image: string;
+  description: string;
+  techStack: Array<string>;
   isFirstItem?: boolean;
-  urlDeploy?:   string;
-  urlGithub?:   string;
+  urlDeploy?: string;
+  urlGithub?: string;
 }
 
-export const projects: Array<IProject> =  [
+export const projects: Array<IProject> = [
   {
-    title:       "CONIN SMT",
-    image:        coninImage,
-    description: "High-impact social project aimed at combating child malnutrition in Argentina. It allows users to make donations through Mercado Pago, in order to provide food and necessary resources to improve the health and well-being of affected children.", 
-    techStack:   ["Javascript", "React", "Firebase", "NodeJS", "Mercado Pago"],
-    urlDeploy:   "https://embajadores-conin.vercel.app/"
+    title: "CONIN SMT",
+    image: coninImage,
+    description:
+      "High-impact social project aimed at combating child malnutrition in Argentina. It allows users to make donations through Mercado Pago, in order to provide food and necessary resources to improve the health and well-being of affected children.",
+    techStack: ["Javascript", "React", "Firebase", "NodeJS", "Mercado Pago"],
+    urlDeploy: "https://embajadores-conin.vercel.app/",
   },
   {
-    title:       "Portamor",
-    image:       portamorImage,
-    description: "Project focused on promoting active aging among individuals through the implementation of an online education platform in Peru. This platform provides learning opportunities and personal development to the elderly population.", 
-    techStack:   ["Javascript", "React", "NodeJS", "PostgreSQL"],
+    title: "Portamor",
+    image: portamorImage,
+    description:
+      "Project focused on promoting active aging among individuals through the implementation of an online education platform in Peru. This platform provides learning opportunities and personal development to the elderly population.",
+    techStack: ["Javascript", "React", "NodeJS", "PostgreSQL"],
   },
   {
-    title:       "Non Fungible Town",
-    image:       nonFungibleTownImage,
-    description: "An NFT Ecommerce platform that provides seamless opportunities for buying and selling NFTs using cryptocurrencies as well as cash payments.", 
-    techStack:   ["Javascript", "React", "Metamask", "NodeJS", "Firebase"],
-    urlGithub:   "https://github.com/seba-pitra/e-commerce-NFT",
-    urlDeploy:   "https://e-commerce-nft.vercel.app/"
+    title: "Non Fungible Town",
+    image: nonFungibleTownImage,
+    description:
+      "An NFT Ecommerce platform that provides seamless opportunities for buying and selling NFTs using cryptocurrencies as well as cash payments.",
+    techStack: ["Javascript", "React", "Metamask", "NodeJS", "Firebase"],
+    urlGithub: "https://github.com/seba-pitra/e-commerce-NFT",
   },
   {
-    title:       "Dog Breeds",
-    image:       dogBreedsImage,
-    description: "User-friendly Single-Page-App that integrated an external dog breed API. The app empowered users to effortlessly filter, sort, and even create new dog breeds, complemented by a seamless pagination feature. This enhanced user experience enabled seamless exploration of a diverse range of dog breeds.", 
-    techStack:   ["Javascript", "React", "Redux", "NodeJS", "PostgreSQL"],
-    urlGithub:   "https://github.com/seba-pitra/PI-DOGS",
-    urlDeploy:   "https://pi-dogs-rouge-beta.vercel.app/"
-  }
-]
+    title: "Dog Breeds",
+    image: dogBreedsImage,
+    description:
+      "User-friendly Single-Page-App that integrated an external dog breed API. The app empowered users to effortlessly filter, sort, and even create new dog breeds, complemented by a seamless pagination feature. This enhanced user experience enabled seamless exploration of a diverse range of dog breeds.",
+    techStack: ["Javascript", "React", "Redux", "NodeJS", "PostgreSQL"],
+    urlGithub: "https://github.com/seba-pitra/PI-DOGS",
+  },
+];
